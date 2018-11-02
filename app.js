502f928ce65e24252a69e62e6d9e9a5bb1fa99bb
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-11-02 09:55:35 +0100

    html general font for smallest devices

---
+++ app.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', function () {
+    const image = document.querySelector('.products__banner');
+    console.log(image);
+
+    function slideIn() {
+        const offsetTop = image.getBoundingClientRect().top + document.documentElement.scrollTop;
+        const slideInAt = (window.scrollY + window.innerHeight) - image.clientHeight / 2;
+        const isHalfShown = slideInAt > offsetTop;
+        if (isHalfShown) {
+            image.classList.add('active');
+        }
+    }
+
+
+    window.addEventListener('scroll', slideIn);
+});
